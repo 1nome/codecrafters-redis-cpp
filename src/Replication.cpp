98b51078a8c9f1b4fd79077f539ed0ca4a0bd29c
1nome <stashfoos@gmail.com>
2025-07-13 18:13:54 +0200

    not waiting for a response fix

--- src/Replication.cpp
+++ src/Replication.cpp
@@ -16,10 +16,19 @@ bool is_slave()
 
 void send_handshake(const int master_fd)
 {
+    constexpr int buffer_size = 4096;
+    char in_buffer[buffer_size];
+
     const std::string str1 = command({"PING"});
     send(master_fd, str1.c_str(), str1.size(), 0);
+    recv(master_fd, in_buffer, buffer_size, 0);
+
     const std::string str2 = command({"REPLCONF", "listening-port", config_key_vals["port"]});
     send(master_fd, str2.c_str(), str2.size(), 0);
+    recv(master_fd, in_buffer, buffer_size, 0);
+
     const std::string str3 = command({"REPLCONF", "capa", "psync2"});
     send(master_fd, str3.c_str(), str3.size(), 0);
+    recv(master_fd, in_buffer, buffer_size, 0);
+
 }
