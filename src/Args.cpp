e6eca02062c5e32bd13bada8c53e2d38324058d1
1nome <stashfoos@gmail.com>
2025-07-13 15:27:51 +0200

    changed arg processing, replicaof support

--- src/Args.cpp
+++ src/Args.cpp
@@ -1,58 +1,31 @@
 #include "Args.h"
-#include <unordered_map>
 #include <string>
+#include <unordered_set>
+
 #include "Database.h"
 
 typedef bool (*Arg)(int, char**, int&);
 
-bool dir(const int argc, char** argv, int& i)
-{
-    i++;
-    if (i >= argc)
-    {
-        return false;
-    }
-    config_key_vals["dir"] = std::string(argv[i]);
-    return true;
-}
-
-bool dbfilename(const int argc, char** argv, int& i)
-{
-    i++;
-    if (i >= argc)
-    {
-        return false;
-    }
-    config_key_vals["dbfilename"] = std::string(argv[i]);
-    return true;
-}
-
-bool port(const int argc, char** argv, int& i)
-{
-    i++;
-    if (i >= argc)
-    {
-        return false;
-    }
-    config_key_vals["port"] = std::string(argv[i]);
-    return true;
-}
-
-const std::unordered_map<std::string, Arg> arg_map = {
-    {"--dir", dir},
-    {"--dbfilename", dbfilename},
-    {"--port", port}
+const std::unordered_set<std::string> simple_args = {
+    "--dir",
+    "--dbfilename",
+    "--port",
+    "--replicaof"
 };
 
 bool process_args(const int argc, char** argv)
 {
-    bool ret = true;
     for (int i = 1; i < argc; i++)
     {
-        if (std::string arg = argv[i]; arg_map.contains(arg))
+        if (std::string arg = argv[i]; simple_args.contains(arg))
         {
-            ret &= arg_map.at(arg)(argc, argv, i);
+            i++;
+            if (i >= argc)
+            {
+                return false;
+            }
+            config_key_vals[arg.substr(2)] = std::string(argv[i]);
         }
     }
-    return ret;
+    return true;
 }
