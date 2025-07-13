12eb32b52d627c1676438fbbe13e2b39b32a6027
1nome <stashfoos@gmail.com>
2025-07-13 18:06:13 +0200

    string fix

--- src/Replication.cpp
+++ src/Replication.cpp
@@ -19,7 +19,7 @@ void send_handshake(const int master_fd)
     const std::string str1 = command({"PING"});
     send(master_fd, str1.c_str(), str1.size(), 0);
     const std::string str2 = command({"REPLCONF", "listening-port", config_key_vals["port"]});
-    send(master_fd, str1.c_str(), str1.size(), 0);
+    send(master_fd, str2.c_str(), str2.size(), 0);
     const std::string str3 = command({"REPLCONF", "capa", "psync2"});
-    send(master_fd, str1.c_str(), str1.size(), 0);
+    send(master_fd, str3.c_str(), str3.size(), 0);
 }
